907697f5777037ad1f6d84f26b7348a4450d3fd5
Shinmera <shinmera@tymoon.eu>
2016-09-15 16:39:19 +0200

    Make use of context in testers

--- tester.lisp
+++ tester.lisp
@@ -8,52 +8,57 @@
 
 (defmacro true (form &optional description &rest format-args)
   `(eval-in-context
+    *context*
     (make-instance 'comparison-result
                    :expression ',form
+                   :result (lambda () ,form)
                    :expected '(not null)
                    :comparison 'typep
                    ,@(when description
-                       `(:description (format NIL ,description ,@format-args))))
-    (lambda () ,form)))
+                       `(:description (format NIL ,description ,@format-args))))))
 
 (defmacro false (form &optional description &rest format-args)
   `(eval-in-context
+    *context*
     (make-instance 'comparison-result
                    :expression ',form
+                   :result (lambda () ,form)
                    :expected 'null
                    :comparison 'typep
                    ,@(when description
-                       `(:description (format NIL ,description ,@format-args))))
-    (lambda () ,form)))
+                       `(:description (format NIL ,description ,@format-args))))))
 
 (defmacro is (comp form expected &optional description &rest format-args)
   (let ((exp (gensym "EXP")))
     `(let ((,exp ,expected))
        (eval-in-context
+        *context*
         (make-instance 'comparison-result
                        :expression ',form
+                       :result (lambda () ,form)
                        :expected ,exp
                        :comparison ',comp
                        ,@(when description
-                           `(:description (format NIL ,description ,@format-args))))
-        (lambda () ,form)))))
+                           `(:description (format NIL ,description ,@format-args))))))))
 
 (defmacro fail (form &optional description &rest format-args)
   `(eval-in-context
+    *context*
     (make-instance 'comparison-result
                    :expression ',form
+                   :result (lambda () (nth-value 1 (ignore-errors ,form)))
                    :expected 'error
                    :comparison 'typep
                    ,@(when description
-                       `(:description (format NIL ,description ,@format-args))))
-    (lambda () (nth-value 1 (ignore-errors ,form)))))
+                       `(:description (format NIL ,description ,@format-args))))))
 
 (defmacro of-type (type form &optional description &rest format-args)
   `(eval-in-context
+    *context*
     (make-instance 'comparison-result
                    :expression ',form
+                   :result (lambda () ,form)
                    :expected ',type
                    :comparison 'typep
                    ,@(when description
-                       `(:description (format NIL ,description ,@format-args))))
-    (lambda () ,form)))
+                       `(:description (format NIL ,description ,@format-args))))))
