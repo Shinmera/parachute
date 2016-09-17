9a3f6e4beea48746bdbca75055a54b288e943e34
Shinmera <shinmera@tymoon.eu>
2016-09-17 16:11:03 +0200

    Added generators

--- compat/fiveam.lisp
+++ compat/fiveam.lisp
@@ -34,8 +34,13 @@
    #:*test-dribble*
    #:for-all
    #:gen-integer
-   #:gen-string
+   #:gen-float
    #:gen-character
+   #:gen-string
+   #:gen-list
+   #:gen-tree
+   #:gen-buffer
+   #:gen-one-element
    ;; running tests
    #:run
    #:explain
@@ -194,7 +199,107 @@
                    :description (format NIL ,@reason))))
 
 ;; Generation
+(defclass generation-result (parachute:parent-result)
+  ((generators :initarg :generators :accessor generators)
+   (guard :initarg :guard :accessor guard)
+   (body :initarg :body :accessor body)))
 
+(defmethod parachute:eval-in-context (context (result generation-result))
+  (loop with failure = NIL
+        for vars = (mapcar #'funcall (generators result))
+        repeat *num-trials*
+        do (setf (fill-pointer (parachute:children result)) 0)
+           ;; in order to avoid processing "uninteresting" results,
+           ;; we simply catch everything and rebind the context and
+           ;; rerun the last trial with the proper context. This may
+           ;; be problematic in the case of side-effects, but I can't
+           ;; think of anything better right now.
+           (let ((parachute:*context* result))
+             (handler-case (apply (body result) vars)
+               (error (err) (setf failure err))))
+        until (or failure
+                  (loop for child across (parachute:children result)
+                        thereis (eql :failed (parachute:status child))))
+        while (apply (guard result) vars)
+        finally (apply (body result) vars)))
+
+(defmacro for-all (bindings &body body)
+  (let ((vars (mapcar #'first bindings)))
+    `(parachute:eval-in-context
+      parachute:*context*
+      (make-instance 'generation-result
+                     :expression '(for-all ,bindings ,@body)
+                     :generators (list ,@(mapcar #'second bindings))
+                     :guard (lambda ,vars
+                              (declare (ignorable ,@vars))
+                              (and ,@(remove NIL (mapcar #'third bindings))))
+                     :body (lambda ,vars
+                             ,@body)))))
+
+(defun gen-integer (&key (max (1+ most-positive-fixnum))
+                         (min (1- most-negative-fixnum)))
+  (lambda () (+ min (random (1+ (- max min))))))
+
+(defun gen-float (&key bound (type 'short-float))
+  (let* ((most-negative (ecase type
+                          (short-float most-negative-short-float)
+                          (single-float most-negative-single-float)
+                          (double-float most-negative-double-float)
+                          (long-float most-negative-long-float)))
+         (most-positive (ecase type
+                          (short-float most-positive-short-float)
+                          (single-float most-positive-single-float)
+                          (double-float most-positive-double-float)
+                          (long-float most-positive-long-float)))
+         (bound (or (coerce bound type) (max most-positive (- most-negative)))))
+    (lambda ()
+      (coerce (ecase (random 2)
+                (0 (random (min most-positive bound)))
+                (1 (- (random (min (- most-negative) bound)))))
+              type))))
+
+(defun gen-character (&key (code-limit char-code-limit)
+                           (code (gen-integer :min 0 :max (1- code-limit)))
+                           (alphanumericp nil))
+  (lambda ()
+    (or (loop for count from 0 to 1000
+              for char = (code-char (funcall code))
+              when (and char (or (not alphanumericp) (alphanumericp char)))
+              do (return char))
+        (error "After 1000 iterations ~S has still not generated ~:[a valid~;an alphanumeric~] character :(."
+               code alphanumericp))))
+
+(defun gen-string (&key (length (gen-integer :min 0 :max 80))
+                        (elements (gen-character))
+                        (element-type 'character))
+  (gen-buffer :length length :elements elements :element-type element-type))
+
+(defun gen-list (&key (length (gen-integer :min 0 :max 10))
+                      (elements (gen-integer :min -10 :max 10)))
+  (lambda ()
+    (loop repeat (funcall length)
+          collect (funcall elements))))
+
+(defun gen-tree (&key (size 20)
+                      (elements (gen-integer :min -10 :max 10)))
+  (labels ((build (&optional (current-depth 0))
+             (let ((leaf (< (random (+ 3 (- size current-depth))) 2)))
+               (if leaf
+                   (funcall elements)
+                   (list (build (+ current-depth 1))
+                         (build (+ current-depth 1)))))))
+    (lambda () (build))))
+
+(defun gen-buffer (&key (length (gen-integer :min 0 :max 50))
+                        (element-type '(unsigned-byte 8))
+                        (elements (gen-integer :min 0 :max (1- (expt 2 8)))))
+  (lambda ()
+    (let ((buffer (make-array (funcall length) :element-type element-type)))
+      (map-into buffer elements))))
+
+(defun gen-one-element (&rest elements)
+  (let ((len (length elements)))
+    (lambda () (nth (random len) elements))))
 
 ;; Running
 (defun test-results (report)
