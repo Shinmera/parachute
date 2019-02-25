Shinmera/parachute

commit f0405a6faec2c7321ad375fe4501c160b02a953f

Error out beforehand if the comparator is not a function

result.lisp
@@ -139,6 +139,12 @@
   (ignore-errors (geq (funcall (comparison result) value expected)
                       (comparison-geq result))))
 
+(defmethod eval-in-context :before (context (result comparison-result))
+  (unless (or (functionp (comparison result))
+              (fboundp (comparison result)))
+    (error "~s is not a function designator and cannot be used for comparison."
+           (comparison result))))
+
 (defmethod eval-in-context (context (result comparison-result))
   (call-next-method)
   (when (eql :unknown (status result))

example.lisp
@@ -72,3 +72,9 @@
   (is = 0.0f0 (read-from-string "0.0"))
   (setf *read-default-float-format* 'double-float)
   (is = 0.0d0 (read-from-string "0.0")))
+
+(define-test bad-var
+  (is eql a 'c))
+
+(define-test bad-test
+  (is something 'a 'a))

commit 335b30dd2c10ba48e78bf07fe842dbefc49ba5c9

Add a CHECK-EVALUATABLE generic function to encapsulate validity tests for results (and possibly others).
This fixes a regression that was introduced by f0405a6faec2c7321ad375fe4501c160b02a953f.

documentation.lisp
@@ -202,7 +202,15 @@ context will be some report object and the thing being tested some kind of
 result or test instance.
 
 See REPORT
-See RESULT")
+See RESULT
+See CHECK-EVALUATABLE")
+
+  (function check-evaluatable
+    "Checks whether the thing is evaluatable in the context and signals an error if not.
+
+This is primarily used to error out early on tests that are specified wrong.
+
+See EVAL-IN-CONTEXT")
 
   (function result-for-testable
     "Returns an appropriate RESULT instance for the given testable and context.

package.lisp
@@ -36,6 +36,7 @@
    #:*parent*
    #:*context*
    #:eval-in-context
+   #:check-evaluatable
    #:result-for-testable
    #:result
    #:expression

parachute.asd
@@ -6,7 +6,7 @@
 
 
 (asdf:defsystem parachute
-  :version "1.2.0"
+  :version "1.3.0"
   :license "Artistic"
   :author "Nicolas Hafner <shinmera@tymoon.eu>"
   :maintainer "Nicolas Hafner <shinmera@tymoon.eu>"

result.lisp
@@ -43,9 +43,13 @@
           (expression result) (status result)
           (description result)))
 
+(defmethod check-evaluatable (context (result result)))
+
 (defmethod eval-in-context :around (context (result result))
   ;; Unless the status is unknown marked we should probably skip.
   (when (eql :unknown (status result))
+    ;; Make sure we are evaluatable.
+    (check-evaluatable context result)
     (let ((start (get-internal-real-time)))
       (unwind-protect
            (call-next-method)
@@ -139,7 +143,7 @@
   (ignore-errors (geq (funcall (comparison result) value expected)
                       (comparison-geq result))))
 
-(defmethod eval-in-context :before (context (result comparison-result))
+(defmethod check-evaluatable (context (result comparison-result))
   (unless (or (functionp (comparison result))
               (fboundp (comparison result)))
     (error "~s is not a function designator and cannot be used for comparison."
@@ -183,11 +187,18 @@
 
 (defmethod value-expected-p ((result multiple-value-comparison-result) value expected)
   (loop for comparison in (comparison result)
-        for value in (value result)
-        for expected in (expected result)
+        for value in value
+        for expected in expected
         always (ignore-errors
                 (geq (funcall comparison value expected)
                      (comparison-geq result)))))
+
+(defmethod check-evaluatable (context (result multiple-value-comparison-result))
+  (dolist (comparison (comparison result))
+    (unless (or (functionp comparison)
+                (fboundp comparison))
+      (error "~s is not a function designator and cannot be used for comparison."
+             comparison))))
 
 (defclass parent-result (result)
   ((results :initform (make-array 0 :adjustable T :fill-pointer T) :accessor results)))

test.lisp
@@ -158,6 +158,8 @@
       (loop for v being the hash-values of index
             collect v))))
 
+(defmethod check-evaluatable (context (test test)))
+
 (defmethod eval-in-context (context (test test))
   (let ((*package* (home test)))
     (with-fixtures (fixtures test)